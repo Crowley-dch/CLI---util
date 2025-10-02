8bf7c6a0160c27e302787ad0a644f4c7a2eaa67e
Crowley-dch <dimadogchayka@gmail.com>
2025-10-03 01:08:10 +0300

    add CLI parser header with args struct & prototypes

--- src/cli_parser.h
+++ src/cli_parser.h
@@ -1,20 +1,25 @@
+// src/cli_parser.h
 #ifndef CLI_PARSER_H
 #define CLI_PARSER_H
 
 #include <stdbool.h>
+#include <stddef.h>
 
 typedef struct {
     char *algorithm;   
-    char *mode;       
+    char *mode;        
     bool encrypt;
     bool decrypt;
     char *key_hex;     
+    char *iv_hex;      
     char *input_path;
     char *output_path; 
 } cli_args_t;
 
 int parse_cli_args(int argc, char **argv, cli_args_t *out);
+
 void free_cli_args(cli_args_t *args);
+
 char *derive_output_filename(const char *input, int is_encrypt);
 
 #endif 
