dbde13778f9deb1eee8798721741ef2bf9f97b55
Crowley-dch <dimadogchayka@gmail.com>
2025-10-10 14:01:33 +0300

    integrate CLI parser, AES modes and file I/O into main CLI utility

--- src/main.c
+++ src/main.c
@@ -1,200 +1,141 @@
-#define _POSIX_C_SOURCE 200809L
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdarg.h>
-
-#include <openssl/rand.h>
-
 #include "cli_parser.h"
 #include "file_io.h"
 #include "ecb.h"
-
 #include "modes/cbc.h"
 #include "modes/cfb.h"
 #include "modes/ofb.h"
 #include "modes/ctr.h"
 
-#define IV_LEN 16
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <openssl/rand.h>
 
-static void log_info(const char *fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
-    fprintf(stderr, "[INFO] ");
-    vfprintf(stderr, fmt, ap);
-    fprintf(stderr, "\n");
-    va_end(ap);
-}
+#define AES_BLOCK_SIZE 16
 
-static void log_error(const char *fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
-    fprintf(stderr, "[ERROR] ");
-    vfprintf(stderr, fmt, ap);
-    fprintf(stderr, "\n");
-    va_end(ap);
-}
-
-int main(int argc, char **argv) {
-    int rc = 0;
+int main(int argc, char *argv[]) {
     cli_args_t args;
-    if ((rc = parse_cli_args(argc, argv, &args)) != 0) {
-        return rc;
+    if (parse_cli_args(argc, argv, &args) != 0) {
+        fprintf(stderr, "Usage: %s --algorithm aes --mode <ecb|cbc|cfb|ofb|ctr> "
+                        "(--encrypt|--decrypt) --key <hex32> [--iv <hex32>] "
+                        "--input <file> [--output <file>]\n", argv[0]);
+        return 1;
     }
 
     unsigned char key[16];
     if (hex_to_bytes(args.key_hex, key, sizeof(key)) != 0) {
-        log_error("Invalid key hex");
-        free_cli_args(&args);
-        return 3;
+        fprintf(stderr, "[ERROR] Invalid key format\n");
+        return 2;
     }
 
-    unsigned char *inbuf = NULL;
-    size_t inlen = 0;
-    rc = read_file(args.input_path, &inbuf, &inlen);
-    if (rc != 0) {
-        log_error("Failed to read input file '%s' (code %d)", args.input_path, rc);
-        free_cli_args(&args);
+    unsigned char iv[AES_BLOCK_SIZE];
+    int iv_from_user = 0;
+    if (args.iv_hex && strlen(args.iv_hex) == 32) {
+        if (hex_to_bytes(args.iv_hex, iv, AES_BLOCK_SIZE) != 0) {
+            fprintf(stderr, "[ERROR] Invalid IV hex\n");
+            return 3;
+        }
+        iv_from_user = 1;
+    }
+
+    unsigned char *inbuf = NULL, *outbuf = NULL;
+    size_t inlen = 0, outlen = 0;
+    int rc = 0;
+
+    if (read_file(args.input, &inbuf, &inlen) != 0) {
+        fprintf(stderr, "[ERROR] Failed to read input file '%s'\n", args.input);
         return 4;
     }
 
-    unsigned char iv[IV_LEN];
+    if (args.encrypt) {
+        if (!strcmp(args.mode, "ecb")) {
+            rc = encrypt_ecb(inbuf, inlen, key, &outbuf, &outlen);
+            if (rc == 0) rc = write_file_atomic(args.output, outbuf, outlen);
+        }
+        else {
+            if (!RAND_bytes(iv, AES_BLOCK_SIZE)) {
+                fprintf(stderr, "[ERROR] RAND_bytes failed\n");
+                return 5;
+            }
+
+            if (!strcmp(args.mode, "cbc"))
+                rc = encrypt_cbc(inbuf, inlen, key, iv, &outbuf, &outlen);
+            else if (!strcmp(args.mode, "cfb"))
+                rc = encrypt_cfb(inbuf, inlen, key, iv, &outbuf, &outlen);
+            else if (!strcmp(args.mode, "ofb"))
+                rc = encrypt_ofb(inbuf, inlen, key, iv, &outbuf, &outlen);
+            else if (!strcmp(args.mode, "ctr"))
+                rc = encrypt_ctr(inbuf, inlen, key, iv, &outbuf, &outlen);
+            else {
+                fprintf(stderr, "[ERROR] Unknown mode: %s\n", args.mode);
+                return 6;
+            }
+
+            if (rc == 0)
+                rc = write_file_with_iv(args.output, iv, outbuf, outlen);
+        }
+    }
+
+else if (args.decrypt) {
     unsigned char *cipher_in = NULL;
     size_t cipher_in_len = 0;
 
-    if (args.decrypt) {
-        if (args.iv_hex) {
-            if (hex_to_bytes(args.iv_hex, iv, IV_LEN) != 0) {
-                log_error("Invalid IV hex");
-                free(inbuf);
-                free_cli_args(&args);
-                return 5;
+    if (!strcmp(args.mode, "ecb")) {
+        rc = decrypt_ecb(inbuf, inlen, key, &outbuf, &outlen);
+    } else {
+        if (!iv_from_user) {
+            if (read_file_with_iv(args.input, iv, &cipher_in, &cipher_in_len) != 0) {
+                fprintf(stderr, "[ERROR] Failed to read IV + ciphertext from %s\n", args.input);
+                rc = 7;
+                goto decrypt_cleanup;
             }
+        } else {
             cipher_in = inbuf;
             cipher_in_len = inlen;
-        } else {
-            if (inlen < IV_LEN) {
-                log_error("Input file too short to contain IV (need %d bytes, got %zu)", IV_LEN, inlen);
-                free(inbuf);
-                free_cli_args(&args);
-                return 6;
-            }
-            memcpy(iv, inbuf, IV_LEN);
-            cipher_in = inbuf + IV_LEN;
-            cipher_in_len = inlen - IV_LEN;
         }
-    }
 
-    unsigned char *outbuf = NULL;
-    size_t outlen = 0;
-
-    const char *mode = args.mode;
-
-    if (args.encrypt) {
-        if (strcmp(mode, "ecb") != 0) {
-            if (RAND_bytes(iv, IV_LEN) != 1) {
-                log_error("Failed to generate IV via RAND_bytes()");
-                free(inbuf);
-                free_cli_args(&args);
-                return 7;
-            }
-        }
-    }
-
-#define CALL_ENC_DEC(mode_name, enc_fn, dec_fn) \
-    do { \
-        if (strcmp(mode, mode_name) == 0) { \
-            if (args.encrypt) { \
-                rc = enc_fn(inbuf, inlen, key, iv, &outbuf, &outlen); \
-            } else { \
-                rc = dec_fn(cipher_in, cipher_in_len, key, iv, &outbuf, &outlen); \
-            } \
-            break; \
-        } \
-    } while (0)
-
-
-    rc = -1; 
-    if (strcmp(mode, "ecb") == 0) {
-        if (args.encrypt) {
-            rc = encrypt_ecb(inbuf, inlen, key, &outbuf, &outlen);
-        } else {
-            rc = decrypt_ecb(cipher_in, cipher_in_len, key, &outbuf, &outlen);
-        }
-    } else if (strcmp(mode, "cbc") == 0) {
-        
-        rc = -1;
-        if (args.encrypt) {
-            rc = encrypt_cbc(inbuf, inlen, key, iv, &outbuf, &outlen);
-        } else {
+        if (!strcmp(args.mode, "cbc"))
             rc = decrypt_cbc(cipher_in, cipher_in_len, key, iv, &outbuf, &outlen);
-        }
-    } else if (strcmp(mode, "cfb") == 0) {
-        if (args.encrypt) {
-            rc = encrypt_cfb(inbuf, inlen, key, iv, &outbuf, &outlen);
-        } else {
+        else if (!strcmp(args.mode, "cfb"))
             rc = decrypt_cfb(cipher_in, cipher_in_len, key, iv, &outbuf, &outlen);
-        }
-    } else if (strcmp(mode, "ofb") == 0) {
-        if (args.encrypt) {
-            rc = encrypt_ofb(inbuf, inlen, key, iv, &outbuf, &outlen);
-        } else {
+        else if (!strcmp(args.mode, "ofb"))
             rc = decrypt_ofb(cipher_in, cipher_in_len, key, iv, &outbuf, &outlen);
-        }
-    } else if (strcmp(mode, "ctr") == 0) {
-        if (args.encrypt) {
-            rc = encrypt_ctr(inbuf, inlen, key, iv, &outbuf, &outlen);
-        } else {
+        else if (!strcmp(args.mode, "ctr"))
             rc = decrypt_ctr(cipher_in, cipher_in_len, key, iv, &outbuf, &outlen);
+        else {
+            fprintf(stderr, "[ERROR] Unknown mode: %s\n", args.mode);
+            rc = 8;
         }
-    } else {
-        log_error("Unsupported mode: %s", mode);
-        free(inbuf);
-        free_cli_args(&args);
-        return 8;
-    }
 
-    if (rc != 0) {
-        log_error("Crypto operation failed (code %d)", rc);
-        free(inbuf);
-        free(outbuf);
-        free_cli_args(&args);
-        return 9;
-    }
-
-    if (args.encrypt) {
-        if (strcmp(mode, "ecb") == 0) {
-            rc = write_file_atomic(args.output_path, outbuf, outlen);
-        } else {
-            size_t tot = IV_LEN + outlen;
-            unsigned char *buf_with_iv = malloc(tot);
-            if (!buf_with_iv) {
-                log_error("Memory allocation failed");
-                free(inbuf); free(outbuf); free_cli_args(&args);
-                return 10;
-            }
-            memcpy(buf_with_iv, iv, IV_LEN);
-            memcpy(buf_with_iv + IV_LEN, outbuf, outlen);
-            rc = write_file_atomic(args.output_path, buf_with_iv, tot);
-            free(buf_with_iv);
-        }
-        if (rc != 0) {
-            log_error("Failed to write output file (code %d)", rc);
-            free(inbuf); free(outbuf); free_cli_args(&args);
-            return 11;
-        }
-    } else {
-        rc = write_file_atomic(args.output_path, outbuf, outlen);
-        if (rc != 0) {
-            log_error("Failed to write output file (code %d)", rc);
-            free(inbuf); free(outbuf); free_cli_args(&args);
-            return 12;
+        if (!iv_from_user) {
+            free(cipher_in);
+            cipher_in = NULL;
         }
     }
 
-    log_info("Operation completed successfully.");
+    if (rc == 0)
+        rc = write_file_atomic(args.output, outbuf, outlen);
+}
+
+decrypt_cleanup:
+    ;
+
+
+    if (rc == 0)
+        printf("[INFO] Operation completed successfully.\n");
+    else
+        fprintf(stderr, "[ERROR] Crypto operation failed (code %d)\n", rc);
+
     free(inbuf);
     free(outbuf);
-    free_cli_args(&args);
-    return 0;
+    return rc;
+
+if (inbuf) {
+    free(inbuf);
+    inbuf = NULL;
+}
+if (outbuf) {
+    free(outbuf);
+    outbuf = NULL;
+}
 }
